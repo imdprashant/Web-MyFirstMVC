8e48b9df7bccfe648c1be3e528902a8d7f55b87d
Prashant <prashant@softtact.com>
2018-03-16 16:42:09 +0530

    Revert "hello"
    This reverts commit db2ee20524d31a8368d2d6d8e2ebfe33af4ec819.

---
+++ DB/SQLQuerys.sql
@@ -0,0 +1,72 @@
+USE [MvcDB]
+GO
+
+CREATE TABLE [dbo].[LOOKUPRole](
+    [LOOKUPRoleID] [int] IDENTITY(1,1) NOT NULL,
+    [RoleName] [varchar](100) DEFAULT '',
+    [RoleDescription] [varchar](500) DEFAULT '',
+    [RowCreatedSYSUserID] [int] NOT NULL,
+    [RowCreatedDateTime] [datetime]  DEFAULT GETDATE(),
+    [RowModifiedSYSUserID] [int] NOT NULL,
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(),
+PRIMARY KEY (LOOKUPRoleID)
+    )
+GO
+
+INSERT INTO LOOKUPRole (RoleName,RoleDescription,RowCreatedSYSUserID,RowModifiedSYSUserID)
+       VALUES ('Admin','Can Edit, Update, Delete',1,1)
+INSERT INTO LOOKUPRole (RoleName,RoleDescription,RowCreatedSYSUserID,RowModifiedSYSUserID)
+       VALUES ('Member','Read only',1,1)
+
+
+CREATE TABLE [dbo].[SYSUser](
+    [SYSUserID] [int] IDENTITY(1,1) NOT NULL,
+    [LoginName] [varchar](50) NOT NULL,
+    [PasswordEncryptedText] [varchar](200) NOT NULL,
+    [RowCreatedSYSUserID] [int] NOT NULL,
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(),
+    [RowModifiedSYSUserID] [int] NOT NULL,
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(),
+    PRIMARY KEY (SYSUserID)
+)
+
+GO
+CREATE TABLE [dbo].[SYSUserProfile](
+    [SYSUserProfileID] [int] IDENTITY(1,1) NOT NULL,
+    [SYSUserID] [int] NOT NULL,
+    [FirstName] [varchar](50) NOT NULL,
+    [LastName] [varchar](50) NOT NULL,
+    [Gender] [char](1) NOT NULL,
+    [RowCreatedSYSUserID] [int] NOT NULL,
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(),
+    [RowModifiedSYSUserID] [int] NOT NULL,
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(),
+    PRIMARY KEY (SYSUserProfileID)
+    )
+GO
+
+ALTER TABLE [dbo].[SYSUserProfile]  WITH CHECK ADD FOREIGN KEY([SYSUserID])
+REFERENCES [dbo].[SYSUser] ([SYSUserID])
+GO
+
+
+CREATE TABLE [dbo].[SYSUserRole](
+    [SYSUserRoleID] [int] IDENTITY(1,1) NOT NULL,
+    [SYSUserID] [int] NOT NULL,
+    [LOOKUPRoleID] [int] NOT NULL,
+    [IsActive] [bit] DEFAULT (1),
+    [RowCreatedSYSUserID] [int] NOT NULL,
+    [RowCreatedDateTime] [datetime] DEFAULT GETDATE(),
+    [RowModifiedSYSUserID] [int] NOT NULL,
+    [RowModifiedDateTime] [datetime] DEFAULT GETDATE(),
+    PRIMARY KEY (SYSUserRoleID)
+)
+GO
+
+ALTER TABLE [dbo].[SYSUserRole]  WITH CHECK ADD FOREIGN KEY([LOOKUPRoleID])
+REFERENCES [dbo].[LOOKUPRole] ([LOOKUPRoleID])
+GO
+
+ALTER TABLE [dbo].[SYSUserRole]  WITH CHECK ADD FOREIGN KEY([SYSUserID])
+REFERENCES [dbo].[SYSUser] ([SYSUserID])
+GO
